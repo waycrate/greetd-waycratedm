ff74979f30668cba28a6edc0d57b702ecaa67bfa
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-10-24 11:32:13 +0800

    chore: support search on local dir

--- src/DesktopListModel.cpp
+++ src/DesktopListModel.cpp
@@ -6,9 +6,31 @@
 DesktopModel::DesktopModel(QObject *parent)
   : QAbstractListModel(parent)
 {
+    QStringList entrys;
+    QDir localwaylandapp("/usr/local/share/wayland-sessions/");
+    if (localwaylandapp.exists()) {
+        QStringList desktops = localwaylandapp.entryList({"*.desktop"}, QDir::Files);
+        for (QString &desktop : desktops) {
+            if (entrys.contains(desktop)) {
+                continue;
+            }
+            entrys.push_back(desktop);
+            auto path = QString("/usr/local/share/wayland-sessions/%1").arg(desktop);
+            QSettings settings(path, QSettings::IniFormat);
+            settings.beginGroup("Desktop Entry");
+            m_infos.append(DesktopInfo{
+              .name = settings.value("Name").toString().trimmed(),
+              .exec = settings.value("Exec").toString().trimmed(),
+            });
+        }
+    }
     QDir waylandapp("/usr/share/wayland-sessions/");
     QStringList desktops = waylandapp.entryList({"*.desktop"}, QDir::Files);
     for (QString &desktop : desktops) {
+        if (entrys.contains(desktop)) {
+            continue;
+        }
+        entrys.push_back(desktop);
         auto path = QString("/usr/share/wayland-sessions/%1").arg(desktop);
         QSettings settings(path, QSettings::IniFormat);
         settings.beginGroup("Desktop Entry");
